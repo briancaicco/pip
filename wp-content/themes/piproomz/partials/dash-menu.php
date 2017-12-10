c691bc8f24cc2451c764cb359a1c0a6221e294b5
Brian Caicco <briancaicco@gmail.com>
2017-12-10 00:29:40 -0500

    tweaks

--- wp-content/themes/piproomz/partials/dash-menu.php
+++ wp-content/themes/piproomz/partials/dash-menu.php
@@ -5,7 +5,7 @@
             <a class="nav-link p-3 " href="<?php bloginfo('url');?>/dashboard">Dashboard</a>
             <a class="nav-link p-3 " href="<?php bloginfo('url');?>/brokers">Brokers</a>
             <a class="nav-link p-3 " href="<?php bloginfo('url');?>/rooms">Rooms</a>
-            <a class="nav-link p-3 " href="<?php bloginfo('url');?>/membership">Membership</a>
+			<a href="#" type="" data-toggle="modal" class="btn m-4 btn-success" data-target="#upgrademodal" style="text-decoration: none !important;" >Upgrade</a>
         </nav>
 	</div>
 </div>
