1c8d1d30b1422587fd17ac786975d8727ebe3eb0
Brian Caicco <briancaicco@gmail.com>
2017-12-10 00:32:12 -0500

    tweaks

--- wp-content/themes/piproomz/partials/dash-broker.php
+++ wp-content/themes/piproomz/partials/dash-broker.php
@@ -1,12 +1,12 @@
-<p class="h5 mb-3">Featured Brokers</p>
+<p class="h5 mb-3">Featured Broker</p>
 <div class="row d-flex align-items-stretch mb-4">
-	<div class="col-md-6">
+<!-- 	<div class="col-md-6">
 		<div class="card broker-box">
 			<div class="card-body p-4 ">
 				<a href="<?php bloginfo( 'url' ); ?>/broker/forex-com/" class=" d-block"><img class="img-fluid" src="<?php bloginfo( 'url' ); ?>/wp-content/uploads/2017/11/forex.png" /></a>
 			</div>
 		</div>
-	</div>
+	</div> -->
 	<div class="col-md-6">
 		<div class="card broker-box">
 			<div class="card-body p-4 ">
