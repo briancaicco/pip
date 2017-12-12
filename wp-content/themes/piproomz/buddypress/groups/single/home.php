4cc77a905b0a02a156b1c167f8a6f7c52685a7ea
Brian Caicco <briancaicco@gmail.com>
2017-12-12 11:52:14 -0500

    chat room config

--- wp-content/themes/piproomz/buddypress/groups/single/home.php
+++ wp-content/themes/piproomz/buddypress/groups/single/home.php
@@ -174,7 +174,7 @@
 				<div class="sticky-top pt-5">
 					<?php
 
-						$groupName = get_bp_group_name();
+						$groupName = bp_get_group_name();
 
 						if( $groupName = 'XPTUSD') : $chatRoomId = 1;
 						elseif( $groupName = 'NZDUSD') : $chatRoomId = 2;
