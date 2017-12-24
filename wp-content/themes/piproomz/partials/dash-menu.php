2954fdd29da87c77c2a9972e0ade0ee430d40b93
Brian Caicco <briancaicco@gmail.com>
2017-12-24 09:24:22 -0500

    learnpress plugin

--- wp-content/themes/piproomz/partials/dash-menu.php
+++ wp-content/themes/piproomz/partials/dash-menu.php
@@ -4,6 +4,7 @@
         <nav class="nav flex-column">
             <a class="nav-link p-3 " href="<?php bloginfo('url');?>/dashboard">Dashboard</a>
             <a class="nav-link p-3 " href="<?php bloginfo('url');?>/brokers">Brokers</a>
+            <a class="nav-link p-3 " href="<?php bloginfo('url');?>/courses">Courses</a>
             <a class="nav-link p-3 " href="<?php bloginfo('url');?>/rooms">Rooms</a>
 			<a href="#" type="" data-toggle="modal" class="btn m-4 btn-success" data-target="#upgrademodal" style="text-decoration: none !important;" >Upgrade</a>
         </nav>
