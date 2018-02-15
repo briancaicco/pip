5804eac93de3f2ef443768e18ffb995e5a3c24b5
Brian Caicco <briancaicco@gmail.com>
2018-02-14 19:40:56 -0500

    brokers page updates

--- wp-content/themes/piproomz/page-templates/brokers.php
+++ wp-content/themes/piproomz/page-templates/brokers.php
@@ -14,14 +14,16 @@ $args = array(
 	'post_type'			=> 'broker',
 	'orderby'			=> 'title', 
 	'order'				=> 'ASC',
-	'meta_query' 		=> array(
-		array(
-			'key' 		=> 'broker_staff_pick',
-			'value'		 => '1',
-		)
-	)
+	'tax_query' => array(
+        array(
+            'taxonomy' => 'category',
+            'field' => 'slug', 
+            'terms' => 'forex-brokers'
+        )
+    )
 );
-$staff_pick_brokers = new WP_query( $args );
+
+$forex = new WP_query( $args );
 
 $post_count = wp_count_posts( 'broker' )->publish;
 
@@ -30,40 +32,41 @@ $args2 = array(
 	'posts_per_page'    =>  -1, 
 	'orderby'			=> 'title', 
 	'order'				=> 'ASC',
-	// 'meta_query' 		=> array(
-	// 	array(
-	// 		'key' 		=> 'broker_staff_pick',
-	// 		'value'		=> '1',
-	// 		'compare' 	=> '!='
-	// 	)
-	// )
+	'tax_query' => array(
+        array(
+            'taxonomy' => 'category',
+            'field' => 'slug', 
+            'terms' => 'cryptocurrency-exchanges'
+        )
+    )
 );
 
 
-$all_brokers = new WP_query( $args2 );
+$crypto = new WP_query( $args2 );
 
 
 global $post;
 ?>
 
-<div class="<?php echo esc_attr( $container ); ?> pt-3 pt-md-5" id="content">
 
+<div class="<?php echo esc_attr( $container ); ?> pt-3 pt-md-5" id="content">
+	<!-- FOREX BROKERS -->
 	<div class="row">
 
 		<div class="col">
 
-			<?php if ( $staff_pick_brokers->have_posts() ) { ?>
+			<?php if ( $forex->have_posts() ) { ?>
 
 			<h1 class="mb-4"><?php the_title(); ?></h1>
-			<h5 class="mt-4">Staff Recommended Brokers</h5>
+			<h5 class="mt-4">Foreign Exchange Brokers (Forex)</h5>
 			<br/>
 
 		</div>
 	</div>
 	<div class="row">
 
-		<?php while( $staff_pick_brokers->have_posts() ) { 
-			$staff_pick_brokers->the_post(); ?>
+		<?php while( $forex->have_posts() ) { 
+			$forex->the_post(); ?>
 
 			<div class="col-12 col-md-6 col-lg-4 ">
 
@@ -99,6 +102,58 @@ global $post;
 
 		</div>
 
+		<!-- CRYPTO BROKERS -->
 
-	</div><!-- Container end -->
-	<?php get_footer(); ?>
+		<div class="row">
+
+			<div class="col">
+
+				<?php if ( $crypto->have_posts() ) { ?>
+
+				<h5 class="mt-4">Cryptocurrency Exchanges</h5>
+				<br/>
+
+			</div>
+		</div>
+		<div class="row">
+
+			<?php while( $crypto->have_posts() ) { 
+				$crypto->the_post(); ?>
+
+				<div class="col-12 col-md-6 col-lg-4 ">
+
+					<div class="card mb-3 border-0 text-center " style="background: #fff;">
+
+						<a href="<?php the_field('broker_url'); ?>">
+							<img class="card-img-top p-5 p-xl-4" src="<?php the_field('broker_logo'); ?>" />
+						</a> 
+
+						<div class="card-body bg-light d-flex flex justify-content-center align-items-end">
+
+							<div class="r">
+								<div class="h4 mb-0 text-secondary sr-only">
+									<?php the_title(); ?>
+								</div>
+								<a class="btn btn-large btn-success px-xl-3 mb-3" href="<?php the_field('broker_url'); ?>">Setup Your Account</a>
+								<!-- <p class="mb-0 small"><a href="<?php // the_permalink();?>">Learn More</a></p> -->
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+
+				<?php } ?>
+
+
+				<?php wp_reset_postdata(); ?> 
+
+				<?php } ?>
+
+			</div>
+
+
+		</div><!-- Container end -->
+		<?php get_footer(); ?>
